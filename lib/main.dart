6e8b5451a59847aa72f1e8196ce7d95abfbe8899
Luis Santacruz <luisantacruzkids@gmail.com>
2023-12-29 22:56:00 -0400

    Actualizacion de la app y conexion firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/page02.dart';
+import 'package:flutter_aplicaccion/page02.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const renuevApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const renuevApp());
+}
 
 // Widget que hace que corra la Aplicacion
 class renuevApp extends StatelessWidget {
